2e88dd61e24414a1294bfb7c34b047b06296a923
Estevao <estevaovasques@hotmail.com>
2017-01-18 19:57:38 -0200

    Adicionado um panel do bootstrap para melhorar a visualização

--- loja/adiciona-produto.php
+++ loja/adiciona-produto.php
@@ -3,6 +3,19 @@
     $nome = $_GET["nome"];
     $preco = $_GET["preco"];
    ?>
-  Produto <?= $nome ?> , <?= $preco?> adicionado com sucesso!
+
+  <div class="row">
+    <div class="col-sm-4">
+      Produto adicionado com sucesso.
+      <div class="panel panel-default">
+        <div class="panel-heading"> <?=$nome ?> </div>
+        <div class="panel-body">
+          R$: <?= number_format($preco, 2)?>
+
+        </div>
+      </div>
+
+    </div>
+  </div>
 
 <?php include("rodape.php") ?>
--- loja/produto-formulario.php
+++ loja/produto-formulario.php
@@ -1,9 +1,14 @@
 <?php include("cabecalho.php") ?>
     <form class="" action="adiciona-produto.php" method="GET">
-      <label for="">Produto</label>
-      <input type="text" name="nome" value="">
-      <label for="">Preço</label>
-      <input type="text" name="preco" value="">
+      <div class="form-group">
+        <label for="">Produto</label>
+        <input type="text" name="nome"  class="form-control" />
+      </div>
+      <div class="form-group">
+
+        <label for="">Preço</label>
+        <input type="text" class="form-control" name="preco" />
+      </div>
 
       <button class="btn btn-primary" type="submit">Cadastrar</button>
     </form>
